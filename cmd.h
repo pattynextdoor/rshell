bfad839b2ea95546b43110c002f8168faa8427e6
andrew-t187 <ato004@ucr.edu>
2017-10-28 20:40:23 -0700

    updated syntax

--- cmd.h
+++ cmd.h
@@ -1,15 +1,15 @@
 #ifndef __COMMAND_H__
 #define __COMMAND_H__
 
-#include <base.h>
+#include "base.h"
 #include <iostream>
 #include <string>
 
 class CMD : public Base {
 	protected:
-	vector<Base*> commands;
+		vector<Base*> commands;
 	
 	public:
-	bool execute();
+		bool execute();
 };
 #endif
