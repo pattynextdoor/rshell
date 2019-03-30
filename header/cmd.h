09b7b5b571d27509a20b96679c908d5902214f3c
Patrick Tumbucon <ptumb001@ucr.edu>
2019-03-30 13:59:20 -0700

    Refactored logical operators

--- header/cmd.h
+++ header/cmd.h
@@ -2,13 +2,35 @@
 #define __CMD_H__
 
 #include "base.h"
+
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <stack>
+#include <queue>
 #include <vector>
+#include <string.h>
+#include <unistd.h>
+#include <fstream>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
 
 class CMD : public Base {
+  private:
+    bool isLogicalOperator(std::vector<string>, unsigned); 
+    std::vector<string> findOperators(std::vector<string>);
+    bool fileExists(string&);
+    bool isDirectory(string&);
+    bool isFile(string&);
   public:
-    vector<string> commands;
+    std::vector<string> commands;
     CMD();
     bool execute();
 };
